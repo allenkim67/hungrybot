47364f86763f167288ec3c7e6a06b8e89044f7bf
Daniel J Lee <danieljoonlee@gmail.com>
2015-11-08 21:18:47 -0800

    purchase new phone number on upgrade account

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router  = express.Router();
 var User    = require('../model/User');
 var bcrypt  = require('bcrypt');
+var twilio  = require('twilio');
+var client  = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
 router.post('/create', function(req, res){
   User.create({username: req.body.username, password: bcrypt.hashSync(req.body.password, 8)}, function(err, user){
@@ -10,12 +12,30 @@ router.post('/create', function(req, res){
   });
 });
 
-router.get('/new', function(req, res){
+router.get('/new', function(req, res) {
   res.render('user/new');
 });
 
 router.get('/upgrade', function(req, res) {
   res.render('user/upgrade');
 });
+
+router.post('/addphone', function (req, res) {
+  client.incomingPhoneNumbers.create({
+    phoneNumber: req.body.phone
+  }, function(err, purchasedNumber) {
+    if(!err){
+      User.findOne({username: req.cookies.username}, function(err, user){
+        user.phone = purchasedNumber.phoneNumber;
+        user.save();
+        res.redirect('/');
+      })
+    } else {
+      res.send(err);
+    } 
+  });
+});
+
+
 
 module.exports = router;
