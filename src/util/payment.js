21e7709aa23ccd92b9faa459bd8a2e91e50d84fb
Allen Kim <allenkim67@gmail.com>
2015-11-10 08:06:33 +0700

    fix bug - get stripeAccount on user

--- src/util/payment.js
+++ src/util/payment.js
@@ -1,16 +1,13 @@
 var stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports.createCustomerId = function(params, mongoCustomer, callback) {
-  console.log('THIS IS THE PARAMS', params);
   var card = {
     "number": params.ccNumber,
     "exp_month": params.ccExpMonth,
     "exp_year": params.ccExpYear,
     "cvc": params.ccCvc
   };
-  console.log('THIS IS THE CREDICARD', card);
   stripe.tokens.create({card: card}, function(err, token) {
-    console.log('THIS IS THE TOKEN', token);
     stripe.customers.create({source: token.id}, function(err, stripeCustomer) {
       mongoCustomer.stripeId = stripeCustomer.id;
       mongoCustomer.save();
@@ -26,6 +23,6 @@ module.exports.makePaymentWithCardInfo = function(amount, customerId, user) {
       currency: "usd",
       source: token.id,
       application_fee: Math.ceil(amount * user.appFee)
-    }, {stripe_account: stripeAccount});
+    }, {stripe_account: user.stripeAccount});
   });
 };
