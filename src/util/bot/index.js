360250a94038841ec80f8646719d8ca454ffe777
Allen Kim <allenkim67@gmail.com>
2015-11-13 11:35:22 +0700

    remove debug changes for ai query

--- src/util/bot/index.js
+++ src/util/bot/index.js
@@ -6,8 +6,7 @@ var Business       = require('../../model/Business');
 
 module.exports = function(phoneData, callback) {
   Business.findOne({phone: phoneData.To}, function(err, business){
-    console.log(business._id);
-    ai.query(phoneData.Body, '5645563e018d861100a4daab').then(function(aiResponse){
+    ai.query(phoneData.Body, business._id).then(function(aiResponse){
       var botResponse = getBotResponse(aiResponse);
       var models = {
         customer: Customer.findOne({phone: phoneData.From}),
