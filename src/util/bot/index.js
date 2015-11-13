601f94a0b3f7605cbbf08ba4cb27562dc371944c
Allen Kim <allenkim67@gmail.com>
2015-11-13 11:44:15 +0700

    convert business id to string

--- src/util/bot/index.js
+++ src/util/bot/index.js
@@ -6,8 +6,7 @@ var Business       = require('../../model/Business');
 
 module.exports = function(phoneData, callback) {
   Business.findOne({phone: phoneData.To}, function(err, business){
-    console.log('\n\n\n', business._id === '5645563e018d861100a4daab', '\n', business._id, '\n', '5645563e018d861100a4daab', '\n\n\n');
-    ai.query(phoneData.Body, '5645563e018d861100a4daab').then(function(aiResponse){
+    ai.query(phoneData.Body, business._id.toString()).then(function(aiResponse){
       var botResponse = getBotResponse(aiResponse);
       var models = {
         customer: Customer.findOne({phone: phoneData.From}),
