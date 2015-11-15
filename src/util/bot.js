2e06ae1684683761ae742fb09b5339599d123dc0
Allen Kim <allenkim67@gmail.com>
2015-11-15 16:56:59 +0700

    add bot demo

--- src/util/bot.js
+++ src/util/bot.js
@@ -47,3 +47,36 @@ module.exports = async function(phoneData) {
   var aiResponse = await ai.query(phoneData.Body, business._id.toString());
   return await getBotResponse(aiResponse, {customer: customer, business: business});
 };
+
+// This is for running the bot in terminal for demo purposes.
+// Must use babel-node.
+if (!module.parent) {
+  var readLineSync = require('readline-sync');
+  var mongoose     = require('mongoose');
+  mongoose.connect('mongodb://localhost/hungrybot');
+
+  var DEMO_NUMBER = '+12136636123';
+
+  var modelsPromise = Promise.all([
+    Business.findOne({phone: process.argv[2]}).exec(),
+    Customer.findOne({phone: DEMO_NUMBER}).exec()
+  ]);
+
+  modelsPromise.then(function(business, customer) {
+    var models = {business: business, customer: customer};
+    var query = readLineSync.question('Say something to wake the bot: \n');
+    var aiResponsePromise = ai.query(query);
+
+    aiResponsePromise.then(function(aiResponse) {
+      getBotResponse(aiResponse, models).then(callback);
+    });
+
+    function callback(botResponse) {
+      query = readLineSync.question(botResponse + '\n');
+      var aiResponsePromise = ai.query(query);
+      aiResponsePromise.then(function(aiResponse) {
+        getBotResponse(aiResponse, models).then(callback);
+      });
+    }
+  });
+}
