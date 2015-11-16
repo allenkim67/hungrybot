34ff75651d72db5084a5e8206abf5e6858af9198
Daniel J Lee <danieljoonlee@gmail.com>
2015-11-15 18:43:15 -0800

    add validation for login

--- src/routes/session.js
+++ src/routes/session.js
@@ -1,9 +1,11 @@
-var express        = require('express');
-var router         = express.Router();
-var Business       = require('../model/Business');
-var bcrypt         = require('bcrypt');
-var jwt            = require('jsonwebtoken');
-var authMiddleware = require('../authMiddleware');
+var express          = require('express');
+var router           = express.Router();
+var Business         = require('../model/Business');
+var bcrypt           = require('bcrypt');
+var jwt              = require('jsonwebtoken');
+var authMiddleware   = require('../authMiddleware');
+var expressValidator = require('express-validator');
+var _                = require('underscore');
 
 router.get('/logout', function(req, res){
   res.clearCookie('session');
@@ -15,14 +17,25 @@ router.get('/login', function(req, res) {
 });
 
 router.post('/login', function(req, res){
+  req.checkBody('name', 'Username cannot be blank').notEmpty();
+  req.checkBody('password', 'Password cannot be blank').notEmpty();  
   var userData = {name: req.body.name};
 
-  Business.findOne(userData, function(err, business){
-    if(bcrypt.compareSync(req.body.password, business.password)){
-      res.cookie('session', jwt.sign(business, process.env.JWT_SECRET_KEY));
-      res.redirect('/');
-    }
-  });
+  req.asyncValidationErrors()
+    .then(function() {
+      Business.findOne(userData, function(err, business){
+        if(bcrypt.compareSync(req.body.password, business.password)){
+          res.cookie('session', jwt.sign(business, process.env.JWT_SECRET_KEY));
+          res.redirect('/');
+        }
+      });     
+    })
+    .catch(function(errors) {
+      res.render('session/login', {
+        errors: errors, 
+        name: _.any(errors, function(error) {return error.param === 'name'}) ? '' : req.body.name, 
+      });
+    });
 });
 
 module.exports = router;
