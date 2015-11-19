08f56581aa270b3d13c3b8be7f1338cfa765b521
Allen Kim <allenkim67@gmail.com>
2015-11-19 14:01:40 +0700

    fix menu editing

--- static/js/editMenu.js
+++ static/js/editMenu.js
@@ -1,52 +1,17 @@
 $('.js-menu-btn-edit').click(function(event){
   var $menu = $(event.target).closest('.js-menu');
   var $menuEdit = $(event.target).parent().siblings('.js-menu-edit');
+
+  var name = $menu.find('.js-menu-name-text').text();
+  var description = $menu.find('.js-menu-description-text').text();
+  var price = $menu.find('.js-menu-price-text').text();
+
+  $menuEdit.find('.js-menu-name-input').val(name);
+  $menuEdit.find('.js-menu-description-input').val(description);
+  $menuEdit.find('.js-menu-price-input').val(price);
+
   $menu.hide();
   $menuEdit.show();
-})
-
-$('.js-menu-btn-delete').click(function(event){
-  var $menuItem = $(event.target).closest('.js-menu');
-  var menuItemId = $menuItem.find('.js-menu-id').data('id');
-  $.ajax({ 
-    type: 'delete',
-    url: '/menu/delete/' + menuItemId,
-  });
-  $menuItem.html('')  
-})
-
-$('.js-menu-btn-save').click(function(event){
-  var $menuEdit = $(event.target).closest('.js-menu-edit');
-  var $menu = $(event.target).parent().siblings('.js-menu');
-  var $menuItem = $(event.target).siblings('.js-menu-item');
-  var $menuNew = $(event.target).parent().siblings('.js-menu').children('.js-menu-id');
-  var name = $menuEdit.find('.js-menu-name').find('input').val();
-  var description = $menuEdit.find('.js-menu-description').find('input').val();
-  var price = $menuEdit.find('.js-menu-price').find('input').val();
-  var menuItemId = $menu.find('.js-menu-id').data('id');
-  $menuEdit.hide();
-  $menu.show();
-  $menuNew.html(
-      '<li>Name: ' + name + '</li>' +
-      '<li>Description: ' + description + '</li>' +
-      '<li>Price: ' + price + '</li>' 
-  );
-  $menuItem.html(
-      '<li class="js-menu-name">' +
-        'Name: <input value='+name+'></input>' +
-      '</li>' +
-      '<li class="js-menu-description">' +
-        'Description: <input value=' + description + '></input>' +
-      '</li>' +
-      '<li class="js-menu-price">' +
-        'Price: <input value=' + price + '></input>' +
-      '</li>'
-  );    
-  $.ajax({ 
-    type: 'put',
-    url: '/menu/update/' + menuItemId,
-    data: {name: name, description: description, price: price}
-  });
 });
 
 $('.js-menu-btn-cancel').click(function(event){
@@ -55,4 +20,41 @@ $('.js-menu-btn-cancel').click(function(event){
   $menuEdit.hide();
   $menu.show();
 });
+
+
+$('.js-menu-btn-delete').click(function(event){
+  var $menuContainer = $(event.target).closest('.js-menu-container');
+  var menuId = $menuContainer.data('id');
+
+  $.ajax({
+    type: 'delete',
+    url: '/menu/delete/' + menuId
+  });
+
+  $menuContainer.html('');
+});
+
+$('.js-menu-btn-save').click(function(event){
+  var $menu = $(event.target).parent().siblings('.js-menu');
+  var $menuEdit = $(event.target).closest('.js-menu-edit');
+  var menuId = $(event.target).closest('.js-menu-container').data('id');
+
+  var name = $menuEdit.find('.js-menu-name-input').val();
+  var description = $menuEdit.find('.js-menu-description-input').val();
+  var price = $menuEdit.find('.js-menu-price-input').val();
+
+  $.ajax({
+    type: 'put',
+    url: '/menu/update/' + menuId,
+    data: {name: name, description: description, price: price * 100}
+  });
+
+  $menu.show();
+  $menuEdit.hide();
+
+  $menu.find('.js-menu-name-text').text(name);
+  $menu.find('.js-menu-description-text').text(description);
+  $menu.find('.js-menu-price-text').text(price);
+});
+
 
