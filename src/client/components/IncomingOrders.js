01efbb0cf5eceded553a6cb25658b1e6a4939ead
Daniel J Lee <danieljoonlee@gmail.com>
2015-11-25 17:22:41 -0800

    added styles to all login pages

--- src/client/components/IncomingOrders.js
+++ src/client/components/IncomingOrders.js
@@ -17,35 +17,39 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div>
-        <h2>Incoming Orders</h2>
-        {this.state.orders.length ? (
-          <ul>
-            {this.state.orders.map(order => {
-              return this.renderOrderGroup(order);
-            })}
-          </ul>
-        ) : null}
+        <h2 className="incoming-header">Incoming Orders</h2>
+        <div className="incoming-order-list">
+          {this.state.orders.length ? (
+            <ul>
+              {this.state.orders.map(order => {
+                return this.renderOrderGroup(order);
+              })}
+            </ul>
+          ) : null}
+        </div>
       </div>
     );
   },
   renderOrderGroup: function(orderGroup) {
     return (
-      <li key={orderGroup._id}>
-        <label>Id: </label>
-        <span>{orderGroup._id}</span>
-        <br/>
-        <label>Status: </label>
-        <span>{orderGroup.status}</span>
-        <br/>
-        <label>Items: </label>
-        <ul>
-          {orderGroup.orders.map(function(order) {
-            return <li key={order._id}>{order.quantity} {order.item}</li>
-          })}
-        </ul>
-        <label>Total: </label>
-        <span>${(orderGroup.total/100).toFixed(2)}</span>
-      </li>
+        <div className="incoming-order">
+          <li key={orderGroup._id}>
+            <label>Id: </label>
+            <span>{orderGroup._id}</span>
+            <br/>
+            <label>Status: </label>
+            <span> {orderGroup.status}</span>
+            <br/>
+            <label>Items: </label>
+            <ul className="incoming-list">
+              {orderGroup.orders.map(function(order) {
+                return <li key={order._id}>{order.quantity} {order.item}</li>
+              })}
+            </ul><br/>
+            <label>Total: </label>
+            <span>${(orderGroup.total/100).toFixed(2)}</span>
+          </li>
+        </div>
     );
   }
 });
