497847f7410e0a0a23aca931fce86673d6e53b41
Allen Kim <allenkim67@gmail.com>
2015-11-27 08:04:00 +0700

    scroll to bottom on message for private demo

--- src/client/components/BotDemo.js
+++ src/client/components/BotDemo.js
@@ -5,17 +5,28 @@ module.exports = React.createClass({
   getInitialState: function() {
     return {messages: []}
   },
+  componentWillUpdate: function() {
+    var node = this.refs.messagesContainer;
+    this.shouldScrollBottom = node.scrollTop + node.offsetHeight === node.scrollHeight;
+  },
+
+  componentDidUpdate: function() {
+    if (this.shouldScrollBottom) {
+      var node = this.refs.messagesContainer;
+      node.scrollTop = node.scrollHeight
+    }
+  },
   render: function() {
     return (
       <div>
         <h2 className="bot-header">Bot Demo</h2>
-        <div className="chat-box">
+        <div ref="messagesContainer" className="chat-box">
           {this.state.messages.map(function(message) {
             return <div dangerouslySetInnerHTML={{__html: message}}></div>;
           })}
         </div>
         <form onSubmit={this.submitMessage}>
-          <input className="chat-form form-control chat-input" autoComplete="off"/>
+          <input ref="chatInput" className="chat-form form-control chat-input" autoComplete="off"/>
           <button className="btn btn-primary chat-btn">Send</button>
         </form>
       </div>
@@ -23,7 +34,7 @@ module.exports = React.createClass({
   },
   submitMessage: function(evt) {
     evt.preventDefault();
-    var input = evt.target.querySelector('input');
+    var input = this.refs.chatInput;
 
     var message = input.value;
     this.setState({messages: this.state.messages.concat(message)});
