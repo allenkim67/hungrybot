3805431ccc81023b1e4d3f9ed39c5dc7ef045518
Allen Kim <allenkim67@gmail.com>
2015-12-12 17:00:54 +0700

    console logs for debugging

--- src/server/util/bot/bot.js
+++ src/server/util/bot/bot.js
@@ -10,7 +10,9 @@ module.exports = async function(input) {
   console.log('INPUT:\n', input);
 
   var transitions = transitionTable[input.nlpData.intent] || transitionTable.NO_MATCH;
+  console.log('1', transitions);
   var transition = await asyncFind(transitions, R.partial(filterByState, [input]));
+  console.log('4', transition);
   if (!transition) transition = transitionTable.NO_MATCH[0];
   var output = await applyOutputFns(transition.output, input);
 
@@ -20,6 +22,8 @@ module.exports = async function(input) {
 };
 
 async function filterByState(input, transition) {
+  console.log('2', input);
+  console.log('3', transition);
   if (typeof transition.state === 'object') {
     return sift(transition.state)(input.convoState);
   } else if (typeof transition.state === 'function') {
--- src/server/util/util.js
+++ src/server/util/util.js
@@ -1,6 +1,8 @@
 module.exports.asyncFind = async function(array, predicate) {
   for (var i = 0; i < array.length; i++) {
     if (await predicate(array[i])) {
+      console.log('5', await predicate(array[i]));
+      console.log('6', array[i]);
       return array[i];
     }
   }
