0e488e728fa22b575ae5f290fc274aa295865c7f
Allen Kim <allenkim67@gmail.com>
2015-12-12 16:08:20 +0700

    minify in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+var webpack = require('webpack');
+
+var prod = process.env.NODE_ENV === 'production';
+
 module.exports = {
   entry: './src/client/client.js',
   output: {
-    filename: './static/js/bundle.js'
+    filename: prod ? './static/js/bundle.min.js' : './static/js/bundle.js'
   },
   module: {
     loaders: [
@@ -14,5 +18,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: prod ? [
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ] : []
 };
