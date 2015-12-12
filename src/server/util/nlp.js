1d37753733192e39df8aa30961d32b1b57fe6908
Allen Kim <allenkim67@gmail.com>
2015-12-12 15:09:03 +0700

    add access for production nlp domain

--- src/server/util/nlp.js
+++ src/server/util/nlp.js
@@ -1,15 +1,15 @@
 var axios = require('axios');
 var Menu  = require('../model/Menu');
 
-var NLP_URL = 'http://localhost:3000';
+var NLP_DOMAIN = process.env.NODE_ENV === 'production' ? 'http://hungry-nlp.herokuapp.com' : 'http://localhost:3000';
 
 module.exports.refreshUserEntities = async function(sessionId){
   var menu = await Menu.find({businessId: sessionId}).exec();
   var food = menu.map(item => [item.name].concat(item.synonyms));
-  return await axios.post(`${NLP_URL}/userEntities/${sessionId}`, {food: food});
+  return await axios.post(`${NLP_DOMAIN}/userEntities/${sessionId}`, {food: food});
 };
 
 module.exports.query = async function(message, sessionId){
-  var res = await axios.get(`${NLP_URL}/query/${sessionId}?message=${message}`);
+  var res = await axios.get(`${NLP_DOMAIN}/query/${sessionId}?message=${message}`);
   return res.data;
 };
