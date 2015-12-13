29e146f66d8d7dfa7a5789377b4396fe5f4442a1
Allen Kim <allenkim67@gmail.com>
2015-12-13 15:39:57 +0700

    fix landing page demo and default business

--- setup.js
+++ setup.js
@@ -4,51 +4,56 @@ require('babel-core/register');
 var mongoose = require('mongoose');
 var Business = require('./src/server/model/Business');
 var Menu     = require("./src/server/model/Menu");
+var nlp      = require('./src/server/util/nlp');
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/hungrybot');
 
 mongoose.connection.on('open', function () {
   var businessData = {
-    email: 'hungryDemoBusiness@hungrybot.io'
+    email: 'hungryDemoBusiness@hungrybot.io',
+    name: 'Krusty Krab'
   };
-  Business.findOrCreate(businessData).then(function(business) {
-    var menu1 = {
-      businessId: business._id,
-      name: 'Krabby Patty',
-      description: 'The classic!',
-      price: 200
-    };
-    var menu2 = {
-      businessId: business._id,
-      name: 'Krusty Combo',
-      description: 'Now you can have it all',
-      price: 399
-    };
-    var menu3 = {
-      businessId: business._id,
-      name: 'Krusty Deluxe',
-      description: 'Pure decadence',
-      price: 300
-    };
-    var menu4 = {
-      businessId: business._id,
-      name: 'Seaweed Salad',
-      description: 'For the health conscious sea-dweller',
-      price: 200
-    };
-    var menu5 = {
-      businessId: business._id,
-      name: 'Coral Bits',
-      description: 'A delicious side dish',
-      price: 200
-    };
-    
-    Promise.all([
-      Menu.update(menu1, {$setOnInsert: menu1}, {upsert: true}).exec(),
-      Menu.update(menu2, {$setOnInsert: menu2}, {upsert: true}).exec(),
-      Menu.update(menu3, {$setOnInsert: menu3}, {upsert: true}).exec(),
-      Menu.update(menu4, {$setOnInsert: menu4}, {upsert: true}).exec(),
-      Menu.update(menu5, {$setOnInsert: menu5}, {upsert: true}).exec()
-    ]).then(function() { process.exit(); });
+  Business.update({email: 'hungryDemoBusiness@hungrybot.io'}, businessData, {upsert: true}).then(function() {
+    Business.findOne({email: 'hungryDemoBusiness@hungrybot.io'}).exec().then(function(business) {
+      var menu1 = {
+        businessId: business._id,
+        name: 'Krabby Patty',
+        description: 'The classic!',
+        price: 200
+      };
+      var menu2 = {
+        businessId: business._id,
+        name: 'Krusty Combo',
+        description: 'Now you can have it all',
+        price: 399
+      };
+      var menu3 = {
+        businessId: business._id,
+        name: 'Krusty Deluxe',
+        description: 'Pure decadence',
+        price: 300
+      };
+      var menu4 = {
+        businessId: business._id,
+        name: 'Seaweed Salad',
+        description: 'For the health conscious sea-dweller',
+        price: 200
+      };
+      var menu5 = {
+        businessId: business._id,
+        name: 'Coral Bits',
+        description: 'A delicious side dish',
+        price: 200
+      };
+
+      Promise.all([
+        Menu.update(menu1, {$setOnInsert: menu1}, {upsert: true}).exec(),
+        Menu.update(menu2, {$setOnInsert: menu2}, {upsert: true}).exec(),
+        Menu.update(menu3, {$setOnInsert: menu3}, {upsert: true}).exec(),
+        Menu.update(menu4, {$setOnInsert: menu4}, {upsert: true}).exec(),
+        Menu.update(menu5, {$setOnInsert: menu5}, {upsert: true}).exec()
+      ]).then(function() { return nlp.refreshUserEntities(business._id); })
+        .then(function() { process.exit(); });
+    })
   });
 });
--- static/js/landingPage.js
+++ static/js/landingPage.js
@@ -1,16 +1,18 @@
 $(".js-chat-demo-form").on('submit', function(evt) {
   evt.preventDefault();
-  var guestMessage = evt.target.querySelector('input').value;
+  var chatInput = $(evt.target).find('input');
   var chatDiv = $(".chat-div");
+  var guestMessage = chatInput.val();
+
   $(".chat-text").append('<div class="clear chat"></div><div class="from-me chat"><p>' + guestMessage + '</p></div>');
-  $("#chat-box").val(''); 
+  chatInput.val('');
   chatDiv.scrollTop(chatDiv[0].scrollHeight);
 
   $.get('/bot/public?message=' + guestMessage, function(botResponse) {
     $(".chat-text").append('<div class="clear chat"></div><div class="from-them chat"><p>' + botResponse + '</p></div>');
     chatDiv.scrollTop(chatDiv[0].scrollHeight);  
   });
-})
+});
 
 $(".js-subscribe-form").on('submit', function(evt) {
   evt.preventDefault();
@@ -22,4 +24,4 @@ $(".js-subscribe-form").on('submit', function(evt) {
 
   var subscribedDiv = $(".js-subscribed-msg");
   subscribedDiv.append('<h2 class="subscribed-msg">Thanks for signing up!</h2>');
-})
+});
